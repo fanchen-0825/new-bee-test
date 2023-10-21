a7fee44df4942791e299827f7e75163adcc8b7a3
FC <FCfactory.com>
2023-10-21 13:25:44 +0800

    ceshi

---
+++ newBeeTest/src/main/java/com/example/newbeetest/NewBeeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.newbeetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBeeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewBeeTestApplication.class, args);
+    }
+
+}
---
+++ newBeeTest/src/test/java/com/example/newbeetest/NewBeeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newbeetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBeeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
